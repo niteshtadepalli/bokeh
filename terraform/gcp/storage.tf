5a7eceb04b6ffbca268cf14253b6448fe0b100b3
Xinwei Zhang <xinweizhang@google.com>
2026-07-27 22:50:35 +0000

    Add Dependency in Terraform

--- terraform/gcp/storage.tf
+++ terraform/gcp/storage.tf
@@ -1,5 +1,6 @@
 data "google_project" "project" {
   project_id = var.project_id
+  depends_on = [google_project_service.enabled_services["cloudresourcemanager.googleapis.com"]]
 }
 
 # GCS Bucket for HTML & Partition Scan Reports
