30829c794dbe75f1c19b844d8ac0aeb110d7153b
Xinwei Zhang <xinweizhang@google.com>
2026-07-31 21:30:57 +0000

    Update README and Add License

--- terraform/gcp/outputs.tf
+++ terraform/gcp/outputs.tf
@@ -1,3 +1,17 @@
+# Copyright 2026 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 output "nat_ip" {
   description = "Static IP address allocated for Cloud NAT (if VPC/NAT was created)."
   value       = var.create_vpc_and_nat ? google_compute_address.nat_ip[0].address : null
